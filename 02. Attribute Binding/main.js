f7d23aa83ec05272b4f69df153735306010094b3
Paula Vaz <pcosv@cin.ufpe.br>
2019-10-10 12:03:12 -0300

    some changes in html tittle

---
+++ 02. Attribute Binding/main.js
@@ -0,0 +1,9 @@
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: "vmSocks-green-onWhite.jpg",
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+    }
+})
--- Attribute Binding/main.js
+++
@@ -1,8 +0,0 @@
-
-var app = new Vue({
-    el: '#app',
-    data: {
-        product: 'Socks',
-        image: "vmSocks-green-onWhite.jpg"
-    }
-})
