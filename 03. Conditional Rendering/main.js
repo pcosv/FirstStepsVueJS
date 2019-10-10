fe07a95c5fdbe7848516c2f7134ce5577b90c415
Paula Vaz <pcosv@cin.ufpe.br>
2019-10-10 12:02:25 -0300

    conditional rendering

---
+++ 03. Conditional Rendering/main.js
@@ -0,0 +1,11 @@
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: "vmSocks-green-onWhite.jpg",
+        //inStock: false
+        inventory: 0,
+        onSale: false
+    }
+})
